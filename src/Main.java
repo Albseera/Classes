fda9932fe43f1109efd59b58939947572d46fb61
Alex <albredmy@gmail.com>
2022-12-20 11:15:55 +0300

    Домашка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Создаем первую книгу
+        Author AgataKrist = new Author("Агата", "Кристи");
+        Book tenLittleNiggers = new Book("Десят негретят", AgataKrist, 1939);
+
+        //Создаем вторую книгу
+        Author MichelLermontov = new Author("Михаил", "Лермонтов");
+        Book Borodino = new Book("Бородино", MichelLermontov, 1850);
+
+        //Вывод данных первой книги
+        System.out.println("Название книги : " + tenLittleNiggers.getName());
+        System.out.println("Год публикации : " + tenLittleNiggers.getDataIssue());
+        System.out.println("Автор : " + tenLittleNiggers.getAuthor());
+        //Меняем дату публикации
+        tenLittleNiggers.setDataIssue(1946);
+        System.out.println("Год публикации : " + tenLittleNiggers.getDataIssue());
+
+        //Вывод данных второй  книги
+        System.out.println("Название книги : " + Borodino.getName());
+        System.out.println("Год публикации : " + Borodino.getDataIssue());
+        System.out.println("Автор : " + Borodino.getAuthor());
+        //Меняем дату публикации
+        Borodino.setDataIssue(1877);
+        System.out.println("Год публикации : " + Borodino.getDataIssue());
+
+
     }
 }
